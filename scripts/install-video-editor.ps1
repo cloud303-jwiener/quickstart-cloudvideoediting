9209f96bba7994927a19221054801d6108941769
Mark Stephens <mrkst@amazon.com>
2017-03-02 21:04:36 -0500

    template now installs blender and teradici without throwing exit code of 1

--- scripts/install-video-editor.ps1
+++ scripts/install-video-editor.ps1
@@ -6,6 +6,25 @@ param(
 
 
 #
+# Install Chrome and AWS CLI
+#
+echo ""
+echo "Install Chocolatey"
+$url = 'https://chocolatey.org/install.ps1'
+iex ((new-object net.webclient).DownloadString($url))
+echo "Install Chrome"
+choco install --limit-output -y GoogleChrome
+echo "Install awscli"
+choco install --limit-output -y awscli
+echo "Install blender"
+choco install --limit-output -y blender
+echo ""
+echo "choco installs complete"
+
+
+$workingDir = 'c:\Users\Administrator\Downloads'
+
+#
 # install GPU drivers
 #
 #       /s = silent
@@ -13,9 +32,11 @@ param(
 #
 # http://nvidia.custhelp.com/app/answers/detail/a_id/2985/~/how-can-i-perform-a-silent-install-of-the-gpu-driver%3F
 #
+
+echo ""
 echo "install GPU drivers"
 date
-Start-Process -FilePath "c:\Users\Administrator\Downloads\369.26-quadro-tesla-grid-winserv2008-2008r2-2012-64bit-international-whql.exe" -ArgumentList "/s /n" -NoNewWindow -Wait
+Start-Process -FilePath "$($workingDir)\362.56-quadro-grid-desktop-notebook-win10-64bit-international-whql.exe" -ArgumentList "/s /n" -NoNewWindow -Wait
 echo "complete: install GPU drivers"
 date
 
@@ -23,32 +44,45 @@ date
 #
 # install teradici software
 #
+echo ""
 echo "installing Teradici"
 date
-Start-Process -FilePath "c:\Users\Administrator\Downloads\PCoIP_agent_release_installer_2.5.1.908_graphics.exe" -ArgumentList "/S /nodeskside" -NoNewWindow -Wait
+#
+# /S for silent
+#
+#
+# ProcessStartInfo is a way to try and trap the Exit 1 coming from the Teradici installer
+#
+$pinfo = New-Object System.Diagnostics.ProcessStartInfo
+$pinfo.FileName = "$($workingDir)\PCoIP_agent_release_installer_2.7.0.4060_graphics.exe"
+$pinfo.RedirectStandardError = $true
+$pinfo.RedirectStandardOutput = $true
+$pinfo.UseShellExecute = $false
+$pinfo.Arguments = "/S /nodeskside"
+$p = New-Object System.Diagnostics.Process
+$p.StartInfo = $pinfo
+$p.Start() | Out-Null
+$p.WaitForExit()
+$stdout = $p.StandardOutput.ReadToEnd()
+$stderr = $p.StandardError.ReadToEnd()
+echo "Teradici process stdout: $stdout"
+echo "Teradici process stderr: $stderr"
+echo "Teradici exit code: " + $p.ExitCode
+#
+# start-process using the method below gives exit code of 1 and
+# causes CF to show a failed stack
+#
+# $obj = Start-Process -PassThru -FilePath "$($workingDir)\PCoIP_agent_release_installer_2.7.0.4060_graphics.exe" -ArgumentList "/S /nodeskside" -NoNewWindow -Wait
+#
 echo "complete: install Teradici"
 date
 
 
-$url = 'https://chocolatey.org/install.ps1'
-iex ((new-object net.webclient).DownloadString($url))
-choco install --limit-output -y GoogleChrome
-choco install --limit-output -y awscli
-
-
-#
-# install windows media player, requirement for premiere
-#
-echo "Install windows media player"
-import-module servermanager
-Add-WindowsFeature Desktop-Experience
+echo "cfn-signal --success --stack $stack  --resource $resource"
+cfn-signal --success true --stack $stack --resource $resource
 
 
 #
 # GPU drivers require reboot
 #
 Restart-Computer
-
-
-echo "cfn-signal --success --stack $stack  --resource $resource"
-cfn-signal --success true --stack $stack --resource $resource
